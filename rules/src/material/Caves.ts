c80337bc954fa6770d91e6a9feacf8117c81b78d
Romain Fromi <romain.fromi@gmail.com>
2021-11-03 12:34:46 +0100

    Use matrix arrays to describe the starting cave of each player

---
+++ rules/src/material/Caves.ts
@@ -0,0 +1,61 @@
+import PlayerColor from '../PlayerColor'
+
+enum Space {
+  EMPTY, HUNTER, TOTEM_ANIMAL, HAND, HAND2
+}
+
+const _ = Space.EMPTY
+const H = Space.HUNTER
+const T = Space.TOTEM_ANIMAL
+const a = Space.HAND
+const b = Space.HAND2
+
+const caves: {[key in PlayerColor]: Space[][]} = {
+  [PlayerColor.Yellow]: [
+    [_, _, _, _, _, _, _],
+    [_, T, _, _, _, _, _],
+    [_, _, _, _, _, b, _],
+    [_, _, H, _, _, _, _],
+    [_, _, _, a, _, _, _],
+    [_, _, _, _, T, _, _],
+    [_, _, _, _, _, _, _]
+  ],
+  [PlayerColor.Blue]: [
+    [_, _, _, _, _, _, _],
+    [_, _, a, _, _, _, _],
+    [_, _, _, _, _, T, _],
+    [_, _, _, _, b, _, _],
+    [_, _, _, H, _, _, _],
+    [_, T, _, _, _, _, _],
+    [_, _, _, _, _, _, _]
+  ],
+  [PlayerColor.Red]: [
+    [_, _, a, _, _, _, _],
+    [_, _, _, _, T, _, _],
+    [_, _, _, _, _, _, _],
+    [_, _, _, H, _, _, _],
+    [_, _, _, _, _, b, _],
+    [_, T, _, _, _, _, _],
+    [_, _, _, _, _, _, _]
+  ],
+  [PlayerColor.Green]: [
+    [_, _, _, _, _, _, _],
+    [_, _, a, _, _, _, _],
+    [_, _, _, _, _, T, _],
+    [_, _, _, H, _, _, _],
+    [_, _, _, _, _, _, _],
+    [_, T, _, _, _, _, _],
+    [_, _, _, _, b, _, _]
+  ],
+  [PlayerColor.White]: [
+    [_, _, _, _, _, _, _],
+    [_, _, T, _, _, _, _],
+    [_, _, _, b, _, _, _],
+    [_, _, _, _, H, _, _],
+    [_, _, _, _, _, _, _],
+    [_, a, _, _, _, T, _],
+    [_, _, _, _, _, _, _]
+  ]
+}
+
+export default caves
