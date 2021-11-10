ed3b655bc940586f4b87462549b5fc3d68ffc6c2
Tho33 <theo.gregorio@laposte.net>
2021-11-10 12:43:21 +0100

    prepare css for fulfill goal animation

--- app/src/board/Cave.tsx
+++ app/src/board/Cave.tsx
@@ -3,14 +3,32 @@ import {css} from "@emotion/react";
 import PlayerColor from "@gamepark/prehistories/PlayerColor";
 import {PlayerView, PlayerViewSelf} from "@gamepark/prehistories/types/PlayerView";
 import {FC} from "react";
-import {caveBorder, caveLeft, caveTop, squareSize} from "../utils/styles";
+import {caveBorder, caveLeft, caveTop, setPercentDimension, squareSize, toAbsolute} from "../utils/styles";
 import Images from "../utils/Images";
 import AnimalTile from "./AnimalTile";
 import TilesDropArea from "./TilesDropArea";
 import {usePlayerId} from "@gamepark/react-client";
+import {cavesSize} from "@gamepark/prehistories/material/Caves";
+import Coordinates from "@gamepark/prehistories/types/Coordinates";
 
 type Props = {
   player: PlayerView | PlayerViewSelf
+}
+
+
+const X:boolean = true
+const _:boolean = false
+let caveExample:boolean[][] | undefined 
+
+enum Borders {Top = 1, Bottom, Left, Right} 
+
+function getBorders(coordinates:Coordinates, cave:boolean[][]):Borders[]{
+  const result:Borders[] = []
+  if (coordinates.y === 6 || (coordinates.y<6 && cave[coordinates.y+1][coordinates.x] === false)) {result.push(Borders.Bottom)}
+  if (coordinates.y === 0 || coordinates.y>0 && cave[coordinates.y-1][coordinates.x] === false) {result.push(Borders.Top)}
+  if (coordinates.x === 6 || coordinates.x<6 && cave[coordinates.y][coordinates.x+1] === false) {result.push(Borders.Right)}
+  if (coordinates.x === 0 || coordinates.x>0 && cave[coordinates.y][coordinates.x-1] === false) {result.push(Borders.Left)}
+  return result
 }
 
 const Cave: FC<Props> = ({player}) => {
@@ -21,9 +39,37 @@ const Cave: FC<Props> = ({player}) => {
       {player.cave.map((paint, index) =>
         <AnimalTile key={index} tile={paint.tile} side={paint.side} css={tilePosition(paint.x, paint.y)}/>
       )}
+      {caveExample !== undefined && 
+        <div css={innerCave}>
+          {caveExample.map((line, y) => 
+            line.map((column, x) => 
+              column === true && <div key={x+"_"+y} css={[toAbsolute, squarePosition(x,y),setPercentDimension(14.2857, 14.2857),drawBorder(getBorders({x,y}, caveExample!))]}></div>
+            )
+          )}
+        </div>}
     </div>
   )
 }
+
+const squarePosition = (x:number, y:number) => css`
+top:${14.2857*y}%;
+left:${14.2857*x}%;
+`
+
+const drawBorder = (borders:Borders[]) => css`
+${borders.includes(Borders.Top) && `border-top:0.5em dashed white;`}
+${borders.includes(Borders.Bottom) && `border-bottom:0.5em dashed white;`}
+${borders.includes(Borders.Left) && `border-left:0.5em dashed white;`}
+${borders.includes(Borders.Right) && `border-right:0.5em dashed white;`}
+`
+
+const innerCave = css`
+  position: absolute;
+  left: ${caveBorder}em;
+  top: ${caveBorder}em;
+  width: ${squareSize * cavesSize}em;
+  height: ${squareSize * cavesSize}em;
+`
 
 const style = css`
   position: absolute;
