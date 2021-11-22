5777773713763cb236fd2c95db14b3554780d05e
Romain Fromi <romain.fromi@gmail.com>
2021-11-22 15:08:29 +0100

    Fix bug: wrong number of cards in other players hand when their deck is low

--- rules/src/moves/DrawCards.ts
+++ rules/src/moves/DrawCards.ts
@@ -26,12 +26,14 @@ export function drawCards(state: GameState) {
 
 export function drawCardsInView(state: GameView, move: DrawCards | DrawCardsView) {
   const player = state.players.find(player => player.color === state.sortedPlayers![0])!
-  player.deck -= playerWillDraw(player)
   if (isPlayerViewSelf(player)) {
     if (!isDrawCardsView(move)) throw 'Error: I should receive the information about the cards that I draw'
+    player.deck -= move.cards.length
     player.hand.push(...move.cards)
   } else {
-    player.hand += playerWillDraw(player)
+    const numberOfCards = playerWillDraw(player);
+    player.deck -= numberOfCards
+    player.hand += numberOfCards
   }
   player.hunting!.huntPhase = HuntPhase.ChangeActivePlayer
 }
