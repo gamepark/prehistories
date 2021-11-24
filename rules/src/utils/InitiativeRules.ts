fd209b08cb8e70cd02ecf31936c2f9ea57e1791d
Tho33 <theo.gregorio@laposte.net>
2021-11-24 11:15:05 +0100

    fix initiative rules : Yellow color was always pick for ties breakings

--- rules/src/utils/InitiativeRules.ts
+++ rules/src/utils/InitiativeRules.ts
@@ -6,10 +6,11 @@ import {getColoredDeck} from "../material/Hunters";
 import PlayerColor from "../PlayerColor";
 import teamPower from "./teamPower";
 
-export function compareInitiative(playerA: Pick<PlayerState, 'played'>, playerB: Pick<PlayerState, 'played'>) {
+export function compareInitiative(playerA: Pick<PlayerState, 'played' | 'color'>, playerB: Pick<PlayerState, 'played' | 'color'>) {
   const powerComparison = teamPower(playerA.played) - teamPower(playerB.played);
   if (powerComparison !== 0) return powerComparison
-  else return teamSpeed(playerA.played) - teamSpeed(playerB.played)
+  else {
+    return teamSpeed(playerB.played, playerB.color) - teamSpeed(playerA.played, playerA.color)}
 }
 
 export function getNextPlayer(state: GameState | GameView) {
@@ -18,10 +19,10 @@ export function getNextPlayer(state: GameState | GameView) {
   return playerWithPlayedCards.reduce((fastestPlayer, currentPlayer) => compareInitiative(fastestPlayer, currentPlayer) > 0 ? currentPlayer : fastestPlayer)
 }
 
-function teamSpeed(hunters: number[]): number {
+function teamSpeed(hunters: number[], color:PlayerColor): number {
   const deck = []
   for (const elem of hunters) {
-    deck.push(getColoredDeck(PlayerColor.Yellow)[elem].speed)
+    deck.push(getColoredDeck(color)[elem].speed)
   }
   return hunters.length === 0 ? 0 : Math.max(...deck)
 }
