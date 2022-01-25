ba471566a3a3a34a4e3d6c6b09efbaad69269483
Tho33 <theo.gregorio@laposte.net>
2022-01-25 18:49:36 +0100

    fix avatars + add bubbles speeches

--- app/src/board/AvatarPanel.tsx
+++ app/src/board/AvatarPanel.tsx
@@ -1,9 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import PlayerColor from "@gamepark/prehistories/PlayerColor";
-import { Player } from "@gamepark/react-client";
+import { Avatar, Player, SpeechBubbleDirection } from "@gamepark/react-client";
 import { Picture } from "@gamepark/react-components";
-import Avatar from "awesome-react-avataaars";
 import { FC, HTMLAttributes } from "react";
 import { useTranslation } from "react-i18next";
 import { toFullSize } from "../utils/styles";
@@ -21,8 +20,8 @@ const AvatarPanel : FC<Props> = ({playerInfo, color, ...props}) => {
     return(
 
         <div {...props} css={[avatarStyle, roundBorders]}>
-            {playerInfo?.avatar 
-                ? <Avatar style={{width:'100%', height:'100%'}} avatarStyle="Circle" {...playerInfo.avatar}/> 
+            {playerInfo?.avatar
+                ? <Avatar playerId={color} css={[toFullSize, roundBorders]} speechBubbleProps={{direction: SpeechBubbleDirection.TOP_LEFT}} /> 
                 : <Picture alt={t('Player avatar')} src={getTotem(color)} css={[toFullSize, roundBorders]} draggable={false}/>
             }
         </div>
@@ -32,6 +31,7 @@ const AvatarPanel : FC<Props> = ({playerInfo, color, ...props}) => {
 }
 
 const avatarStyle = css`
+    position:relative;
     float:left;
     margin:1em 1em;
     height:6em;
@@ -40,6 +40,7 @@ const avatarStyle = css`
 
 const roundBorders = css`
     border-radius:100%;
+    color:black;
 `
 
 export default AvatarPanel
